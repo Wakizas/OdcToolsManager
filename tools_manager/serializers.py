f11a37dc6dd1e5e1c42edeb1f0a0981d6f311f86
wakizas <melvindogbo@gmail.com>
2022-04-28 22:21:50 +0000

    Correction du serializer du model material

--- tools_manager/serializers.py
+++ tools_manager/serializers.py
@@ -3,8 +3,8 @@ from tools_manager.models import Material
 
 
 class MaterialSerializer(serializers.ModelSerializer):
-    material_type = serializers.CharField(source='material_type')
-    entity = serializers.CharField(source='entity')
+    material_type_label = serializers.CharField(source='material_type.label', read_only=True)
+    entity_name = serializers.CharField(source='entity.name', read_only=True)
     class Meta:
         model = Material
-        fields = ['name', 'charateristics', 'equipment_condition', 'material_type', 'entity']
+        fields = ['name', 'characteristics', 'equipment_condition', 'material_type_label', 'entity_name']
