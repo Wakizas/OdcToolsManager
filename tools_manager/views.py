441c68a4aba13a22b607ac86e83b4a8f827832dd
wakizas <melvindogbo@gmail.com>
2022-04-28 19:16:21 +0000

    Création de la vue pour afficher la liste des matériels

--- tools_manager/views.py
+++ tools_manager/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from tools_manager.models import Material
+from tools_manager.serializers import MaterialSerializer
 
-# Create your views here.
+class MaterialList(generics.ListAPIView):
+    serializer_class = MaterialSerializer
+    
+    def get_queryset(self):
+        materials = Material.objects.all()
+        return materials
